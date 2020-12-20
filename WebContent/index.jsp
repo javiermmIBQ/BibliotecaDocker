c0fd346ba82b54dfa94c35ff35a5e7350726e019
Javier Méndez <eltxabi@gmail.com>
2020-12-20 01:13:58 +0100

    Insert Libro added

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,12 +1,56 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="model.Libro" %>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
+<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-<h1>Hola mundo</h1>
+	 <% String error = (String)request.getAttribute("error");
+	 	if (error!=null){ %>
+	 		<p><%=error%></p>
+		<%	 
+	 }%>
+
+	<table> 
+   		<tr> 
+          <th><b>ISBN</b></th> 
+          <th><b>Título</b></th> 
+          <th><b>Autor</b></th> 
+         </tr>
+         
+         <% ArrayList<Libro> libros=(ArrayList<Libro>)request.getAttribute("libros"); 
+	    	if (libros != null){	
+	         for(Libro l:libros){%> 
+	            <tr> 
+	                <td><%=l.getIsbn()%></td> 
+	                <td><%=l.getTitulo()%></td> 
+	                <td><%=l.getAutor()%></td> 
+	            </tr> 
+	            
+	         <%}
+	         }%> 
+    </table>  
+    
+    <div>
+	  <form action="insertar">
+	    <label for="isbn">ISBN</label>
+	    <input type="text" id="isbn" name="isbn" placeholder="ISBN..">
+	
+	    <label for="titulo">Título</label>
+	    <input type="text" id="titulo" name="titulo" placeholder="Titulo..">
+	
+	    <label for="autor">Autor</label>
+	    <input type="text" id="autor" name="autor" placeholder="Autor..">
+	  
+	    <input type="submit" value="Submit">
+	  </form>
+	</div>
+
 </body>
 </html>
---
+++ src/controller/BibliotecaController.java
@@ -0,0 +1,76 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Libro;
+import model.LibroDAO;
+
+/**
+ * Servlet implementation class BibliotecaController
+ */
+@WebServlet(urlPatterns ={"","/insertar"})
+public class BibliotecaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BibliotecaController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		// TODO Auto-generated method stub
+		RequestDispatcher despachador = null;
+	
+		if (request.getServletPath().equals("")) {
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				ArrayList<Libro> libros;
+				libros = libroDAO.getLibros();
+				request.setAttribute("libros", libros);
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("error",e.getMessage());
+			}
+			despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+						
+		} else if (request.getServletPath().equals("/insertar")) {
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				Libro libro = new Libro(Integer.parseInt(request.getParameter("isbn")),request.getParameter("titulo"),request.getParameter("autor"));
+				libroDAO.insertar(libro);
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("error",e.getMessage());
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("error",e.getMessage());
+			}
+			despachador = request.getServletContext().getRequestDispatcher("/");
+		}
+		
+		despachador.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
